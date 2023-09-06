3217f80d12748fd901c87b175304c36751fb2846
Allen Winter <allen.winter@kdab.com>
2023-09-06 10:53:34 -0400

    Add license and copyright headers -- REUSE compliant

--- src/toolbar_p.h
+++ src/toolbar_p.h
@@ -1,3 +1,13 @@
+/*
+  This file is part of KDToolBars.
+
+  SPDX-FileCopyrightText: 2022-2023 Klarälvdalens Datakonsult AB, a KDAB Group company <info@kdab.com>
+
+  SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only
+
+  Contact KDAB at <info@kdab.com> for commercial licensing options.
+*/
+
 #pragma once
 
 #include "toolbar.h"
