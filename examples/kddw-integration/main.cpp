f6f48d043023e6a44e0c878167813a5908034d46
Mauro Persano <mauro.persano@kdab.com>
2023-09-12 17:32:23 -0300

    Added example showing how to integrate KDToolBars with KDDockWidgets
    We create a KDDockWidgets main window inside a KDToolBars main window.
    
    Change-Id: I23d3127044ce071634721a650a502cc9a9771cd9
    Reviewed-on: https://codereview.kdab.com/c/kdab/kdtoolbars/+/130672
    Reviewed-by: Mauro Persano <mauro.persano@kdab.com>

---
+++ examples/kddw-integration/main.cpp
@@ -0,0 +1,92 @@
+/*
+  This file is part of KDToolBars.
+
+  SPDX-FileCopyrightText: 2022-2023 Klarälvdalens Datakonsult AB, a KDAB Group company <info@kdab.com>
+
+  SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only
+
+  Contact KDAB at <info@kdab.com> for commercial licensing options.
+*/
+
+#include <kdtoolbars/mainwindow.h>
+#include <kdtoolbars/toolbar.h>
+
+#include <kddockwidgets/DockWidget>
+#include <kddockwidgets/MainWindow>
+
+#include <QApplication>
+#include <QAction>
+#include <QLabel>
+
+KDToolBars::ToolBar *makeToolBar(const char *name, std::initializer_list<const char *> icons, QWidget *parent = nullptr)
+{
+    auto *toolbar = new KDToolBars::ToolBar(KDToolBars::ToolBarOption::None, parent);
+    toolbar->setWindowTitle(name);
+    for (const auto *iconName : icons) {
+        if (iconName) {
+            auto *action = new QAction(QIcon(QStringLiteral(":/%1").arg(iconName)), iconName, parent);
+            toolbar->addAction(action);
+        } else {
+            toolbar->addSeparator();
+        }
+    }
+    return toolbar;
+}
+
+class TestWindow : public KDToolBars::MainWindow
+{
+public:
+    explicit TestWindow(QWidget *parent = nullptr)
+        : m_dockWidgetWindow(new KDDockWidgets::MainWindow(
+            "KDDW_MainWindow", KDDockWidgets::MainWindowOption_HasCentralWidget, this))
+    {
+        setWindowTitle(tr("KDToolBars example"));
+
+        setCentralWidget(m_dockWidgetWindow);
+
+        createCentralWidget();
+        createToolBars();
+        createDockWidgets();
+    }
+
+private:
+    void createToolBars()
+    {
+        auto *tb1 = makeToolBar("toolbar 1", { "coffee", "globe", nullptr, "sun", "moon", nullptr, "cloud", "cloud-rain" });
+        auto *tb2 = makeToolBar("toolbar 2", { "feather", "upload", "download" });
+        auto *tb3 = makeToolBar("toolbar 3", { "file", "folder", "star", nullptr, "arrow-left", "arrow-up", "arrow-down", "arrow-right" });
+        auto *tb4 = makeToolBar("toolbar 4", { "music", "image", "video", "file-text" });
+
+        addToolBar(tb1);
+        addToolBar(tb2);
+        addToolBar(tb3);
+        insertToolBarBreak(tb3);
+        addToolBar(tb4);
+    }
+
+    void createDockWidgets()
+    {
+        auto *dock = new KDDockWidgets::DockWidget(QStringLiteral("KDDW_Dock"));
+        dock->setWidget(new QLabel(tr("Dock Widget"), this));
+        m_dockWidgetWindow->addDockWidget(dock, KDDockWidgets::Location_OnRight);
+    }
+
+    void createCentralWidget()
+    {
+        auto *centralWidget = new QLabel(tr("Central Widget"), this);
+        m_dockWidgetWindow->setPersistentCentralWidget(centralWidget);
+    }
+
+    KDDockWidgets::MainWindowBase *m_dockWidgetWindow;
+};
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+
+    TestWindow w;
+    w.resize(1500, 400);
+    w.show();
+
+    return app.exec();
+}
