a093c3a9599782f1c294475af9223dcd2e7a7bf3
Mauro Persano <mauro.persano@kdab.com>
2023-09-13 08:43:54 -0300

    Add more toolbar serialization tests
    Change-Id: Ib351c11f4443a47193bb85c9b578d9ba43e84a5b
    Reviewed-on: https://codereview.kdab.com/c/kdab/kdtoolbars/+/130703
    Tested-by: Continuous Integration <build@kdab.com>
    Reviewed-by: Mauro Persano <mauro.persano@kdab.com>

--- tests/tst_mainwindow.cpp
+++ tests/tst_mainwindow.cpp
@@ -11,6 +11,7 @@
 #include <kdtoolbars/toolbar.h>
 #include <kdtoolbars/mainwindow.h>
 
+#include <QAction>
 #include <QTest>
 #include <QSignalSpy>
 
@@ -79,13 +80,14 @@ void TestMainWindow::testSaveState()
     MainWindow mw;
 
     ToolBar tb;
-    tb.setObjectName("test-toolbar"); // needs unique object name for proper serialization
+    tb.setObjectName("test-toolbar"); // needs unique object name for serialization
 
     // add toolbar to the bottom tray
     mw.addToolBar(ToolBarTray::Bottom, &tb);
     QCOMPARE(mw.toolBarCount(), 1);
     QCOMPARE(mw.toolBarTray(&tb), ToolBarTray::Bottom);
 
+    // save state
     auto state = mw.saveToolBarState();
 
     // move toolbar to the right tray
@@ -97,6 +99,36 @@ void TestMainWindow::testSaveState()
     mw.restoreToolBarState(state);
     QCOMPARE(mw.toolBarCount(), 1);
     QCOMPARE(mw.toolBarTray(&tb), ToolBarTray::Bottom);
+
+    // add two actions to the toolbar
+    QAction a1;
+    a1.setObjectName("test-action-1"); // needs unique object name for serialization
+
+    QAction a2;
+    a2.setObjectName("test-action-2");
+
+    tb.addAction(&a1);
+    tb.addAction(&a2);
+    QCOMPARE(tb.actions(), QList<QAction *>({ &a1, &a2 }));
+
+    // save state
+    state = mw.saveToolBarState();
+
+    // swap the action positions
+    tb.insertAction(&a1, &a2);
+    QCOMPARE(tb.actions(), QList<QAction *>({ &a2, &a1 }));
+
+    // restore state, actions should be back to the initial position
+    mw.restoreToolBarState(state);
+    QCOMPARE(tb.actions(), QList<QAction *>({ &a1, &a2 }));
+
+    // remove one of the actions
+    tb.removeAction(&a1);
+    QCOMPARE(tb.actions(), QList<QAction *>({ &a2 }));
+
+    // restore state, removed action should be back
+    mw.restoreToolBarState(state);
+    QCOMPARE(tb.actions(), QList<QAction *>({ &a1, &a2 }));
 }
 
 QTEST_MAIN(TestMainWindow)
